f4a70c737be9d0faf2a7fc3ae765b4e188af0b72
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-09-19 12:04:09 -0300

    backend dependencies installation and initial api config setup

---
+++ Backend/Api/App.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const myLogger = function (req, res, next) {
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.body);
+    next();
+};
+
+app.use(myLogger);
+
+
+app.get('/', function (req, res, next) {
+    res.send({ version: 1.0 });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('app listening on port 5000');
+});
