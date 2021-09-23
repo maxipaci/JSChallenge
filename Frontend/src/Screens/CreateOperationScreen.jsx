e385651aa450127f6627ef52c81388ad6ced542e
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-09-23 10:53:59 -0300

    adding validations to create operation screen

--- Frontend/src/Screens/CreateOperationScreen.jsx
+++ Frontend/src/Screens/CreateOperationScreen.jsx
@@ -6,6 +6,7 @@ import { View, StyleSheet, TextInput, Text, TouchableOpacity} from 'react-native
 import Selector from '../Components/Selector';
 import { HttpClient } from '../services/HttpClient';
 import PostObserver from '../Events/Http/HttpObserver.js';
+import { Validator } from '../Utils/Validator';
 
 const postObserver = PostObserver.getInstance();
 const options = [
@@ -26,7 +27,13 @@ export default class CreateOperationScreen extends React.Component {
           startDate : new Date(),
           typeId: 1,
           amount: 0,
-          concept: ""
+          concept: "",
+          colorInputAmount: '#D1D1D1',
+          amountCorrectionTxt: "",
+          isValidAmount: false,
+          colorInputConcept: '#D1D1D1',
+          cocneptCorrectionTxt: "",
+          isValidConcept: false
         }
         this.goBack = this.goBack.bind(this);
         this.postOperation = this.postOperation.bind(this);
@@ -58,6 +65,30 @@ export default class CreateOperationScreen extends React.Component {
         this.setState({typeId : parseInt(typeId)})
       }
 
+      onAmountChange(amount){
+        if(amount.length == 0){
+          this.setState({colorInputAmount: "#D1D1D1", amount: 0, amountCorrectionTxt: "", isValidAmount: false})
+        }
+        else if(Validator.isValidAmount(amount)){
+          this.setState({colorInputAmount: "#4DB748", amount: parseInt(amount), amountCorrectionTxt: "", isValidAmount: true})
+
+        } else {
+          this.setState({colorInputAmount: "red", amountCorrectionTxt: "Monto Erroneo", isValidAmount: false})
+        }
+      }
+
+      onConceptChange(concept){
+        if(concept.length == 0){
+          this.setState({colorInputConcept: "#D1D1D1", concept: 0, conceptCorrectionTxt: "", isValidConcept: false})
+        }
+        else if(Validator.isValidConcept(concept)){
+          this.setState({colorInputConcept: "#4DB748", concept: concept, conceptCorrectionTxt: "", isValidConcept: true})
+
+        } else {
+          this.setState({colorInputConcept: "red", conceptCorrectionTxt: "Concepto Erroneo", isValidConcept: false})
+        }
+      }
+
       render(){
         const ExampleCustomInput = React.forwardRef(({ value, onClick }, ref) => (
           <TouchableOpacity style={styles.dateInput} onPress={onClick} ref={ref}>
@@ -69,25 +100,41 @@ export default class CreateOperationScreen extends React.Component {
             <View style={styles.modalView}>
               <View style={styles.formContainer}>
                 <Text style={styles.tittle}>Crear Operacion</Text>
-                <View>
-                  <Text style={styles.butonTittle}>Monto</Text>
+                <View style = {styles.correctionTxtContainer}>
+                  <Text style ={{color: "red"}}>
+                    {this.state.amountCorrectionTxt}
+                  </Text>
                 </View>
+                
+                
                 <TextInput 
-                  style={styles.input}
+                  style={[styles.input,
+                     {
+                       border: "2px solid " + this.state.colorInputAmount
+                      }]}
                   placeholder="600"
-                  onChangeText={e => {this.setState({amount : parseInt(e)})}}
+                  onChangeText={(e) => this.onAmountChange(e)}
                 />
-                <View></View>
+                
+
                 <DatePicker
                   selected={this.state.startDate}
                   onChange={(date) => this.setState({startDate : date})}
                   customInput={<ExampleCustomInput />}
                 />
-                <View></View>
+                <View style = {styles.correctionTxtContainer}>
+                  <Text style ={{color: "red"}}>
+                    {this.state.conceptCorrectionTxt}
+                  </Text>
+                </View>
+                
                 <TextInput 
-                  style={styles.input}
+                  style={[styles.input,
+                    {
+                      border: "2px solid " + this.state.colorInputConcept
+                     }]}
                   placeholder="Comida"
-                  onChangeText={e => {this.setState({concept : e})}}
+                  onChangeText={(e) => this.onConceptChange(e)}
                 />
                 <View></View>
                 <Selector
@@ -97,8 +144,10 @@ export default class CreateOperationScreen extends React.Component {
                 />
                 <View style={styles.buttonContainer}>
                   <TouchableOpacity 
-                    style={styles.saveButton}
+                    style={[styles.saveButton, {backgroundColor: 
+                      !this.state.isValidAmount || !this.state.isValidConcept ? '#D1D1D1' : "green"}]}
                     onPress={this.postOperation}
+                    disabled={!this.state.isValidAmount || !this.state.isValidConcept}
                   >
                     <Text style={styles.butonTittle}>Guardar</Text>
                   </TouchableOpacity>
@@ -179,5 +228,11 @@ const styles = StyleSheet.create({
       borderRadius: "5px",
       justifyContent: "center",
       alignItems: "center"
+  },
+  correctionTxtContainer:{
+      width: "100%",
+      flexDirection: "row",
+      alignItems:"flex-start",
+      justifyContent: "flex-start"
   }
 })
---
+++ Frontend/src/Utils/Validator.js
@@ -0,0 +1,10 @@
+export class Validator{
+    static isValidAmount(amount){
+        return !isNaN(amount) && amount > 0;
+    }
+
+
+    static isValidConcept(concept){
+        return concept.length > 0 && concept.length < 10;
+    }
+}
