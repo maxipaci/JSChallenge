bf5c2db49f872d30954df9224a81ef206c480a98
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-09-26 02:24:33 -0300

    adding InMemory repositories npm script

--- Backend/Infraestructure/Data/Repositories/RepositoryProvider.js
+++ Backend/Infraestructure/Data/Repositories/RepositoryProvider.js
@@ -7,17 +7,20 @@ const config = require(process.cwd() + '\\Config.js');
 
 class RepositoryProvider {
     constructor() {
-        /* this.operationRepo = new InMemoryOperationsRepo();
-        this.operationTypeRepo = new InMemoryOperationTypesRepo(); */
-
-
-        this.db = new Sequelize({
-            dialect: 'sqlite',
-            storage: process.cwd() + "\\Infraestructure\\Data\\Storage\\" + config.dbName
-        });
-        this.operationRepo = new SQLiteOperationsRepo(this.db);
-        this.operationTypeRepo = new SQLiteOperationTypesRepo(this.db);
-        
+        if(process.env.NODE_ENV == 'inmemory '){
+            this.operationRepo = new InMemoryOperationsRepo();
+            this.operationTypeRepo = new InMemoryOperationTypesRepo();
+            console.log("running server with inmemory repositories");
+        } else {
+            this.db = new Sequelize({
+                dialect: 'sqlite',
+                storage: process.cwd() + "\\Infraestructure\\Data\\Storage\\" + config.dbName
+            });
+            this.operationRepo = new SQLiteOperationsRepo(this.db);
+            this.operationTypeRepo = new SQLiteOperationTypesRepo(this.db);
+            console.log("running server with sqlite database");
+        }
+         
     }
 
     static getInstance() {
